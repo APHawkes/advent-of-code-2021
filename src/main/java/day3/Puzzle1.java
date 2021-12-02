77969477fe9030f9d500dbb292cd3ae4d4798989
Adam Hawkes <adam.hawkes@jbhunt.com>
2021-12-02 12:42:54 -0600

    Prep for day 3.

---
+++ src/main/java/day3/Puzzle1.java
@@ -0,0 +1,16 @@
+package day3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Puzzle1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./src/main/resources/day3/input.txt"));
+        String line;
+        int result = 0;
+        while ((line = reader.readLine()) != null) {
+        }
+        System.out.println(result);
+    }
+}
