77073e1c4133125f1b4980f4a5e564dfda08bdb3
Adam Hawkes <adam.hawkes@jbhunt.com>
2021-12-03 11:15:00 -0600

    Day 4 Prep

---
+++ src/main/java/day4/Puzzle1.java
@@ -0,0 +1,17 @@
+package day4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Puzzle1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./src/main/resources/day4/sample.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+
+        }
+
+    }
+}
