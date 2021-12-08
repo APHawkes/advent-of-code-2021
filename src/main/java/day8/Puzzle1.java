720d423e1efb7bf5fa77097ec888feedf3270832
Adam Hawkes <adam.hawkes@jbhunt.com>
2021-12-08 09:37:59 -0600

    Day 8 Puzzle 1 prep

---
+++ src/main/java/day8/Puzzle1.java
@@ -0,0 +1,40 @@
+package day8;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Puzzle1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./src/main/resources/day8/sample.txt"));
+        String currentLine = reader.readLine();
+
+        List<Integer> positions = new ArrayList<>();
+        positions.addAll(Arrays.stream(currentLine.split(",")).map(Integer::parseInt).collect(Collectors.toList()));
+
+        var maxPos = positions.stream().max(Integer::compareTo).get();
+        int minCost = Integer.MAX_VALUE;
+
+        for (int i = 0; i < maxPos; i++) {
+            int cost = calculateCost(positions, i);
+            if (cost < minCost) {
+                minCost = cost;
+            }
+        }
+
+        System.out.println("Min Fuel Cost: " + minCost);
+
+    }
+
+    public static int calculateCost(List<Integer> positions, int finalPosition) {
+        var cost = positions.stream()
+                .map(x-> finalPosition-x)
+                .map(Math::abs)
+                .reduce((x,y)->x+y);
+        return cost.get();
+    }
+}
